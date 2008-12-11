bbd6ff26311d6d2fe2f610b79e7130dc535992b3
benstraub <ben@straubnet.net>
2008-12-11 13:51:01 -0800

    Personal config.

---
+++ bs-init.el
@@ -0,0 +1,38 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; C++ stuff
+
+;; Wacom styleguide-compliant indentation
+(defconst wacom-c-style
+  '((c-basic-offset . 3)
+    (tab-width . 3)
+    (indent-tabs-mode . t)
+    (c-comment-only-line-offset . 0)
+    (c-hanging-braces-alist . ((substatement-open before after)
+                               (defun-open before after)))
+    (c-offsets-alist . ((topmost-intro        . 0)
+                        (substatement         . +)
+                        (substatement-open    . 0)
+                        (case-label           . +)
+                        (access-label         . --)
+                        (inclass              . +)
+                        (inline-open          . 0)
+                        (statement-case-open  . 0))))
+  "Wacom styleguide indentation style")
+(c-add-style "wacom" wacom-c-style)
+
+;; Modified regex for msbuild errors with spaces in the file name
+(add-to-list
+ 'compilation-error-regexp-alist-alist
+ '(msft "^ *\\([0-9]+>\\)?\\(\\(?:[a-zA-Z]:\\)?[^:(]+\\)(\\([0-9]+\\)): \\(?:[a-zA-Z ]*error\\|warnin\\(g\\)\\) C[0-9]+:" 2 3 nil
+        (4)))
+
+;; Aesthetics
+(when (eq window-system 'w32) 
+  (add-to-list 'default-frame-alist
+        '(font . "-outline-Consolas-normal-r-normal-normal-12-82-96-96-c-*-iso8859-1")))
+(when (eq window-system 'mac) 
+  (add-to-list 'default-frame-alist
+        '(font . "-*-andale mono-medium-r-*--12-*-*-*-*-*-mac-roman")))
+(zenburn)
+
+(provide 'bs-init)
---
+++ customized.el
@@ -0,0 +1,26 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(c-basic-offset 3)
+ '(c-default-style (quote ((c-mode . "wacom") (c++-mode . "wacom") (java-mode . "java") (awk-mode . "awk") (other . "gnu"))))
+ '(compilation-ask-about-save nil)
+ '(compilation-scroll-output (quote first-error))
+ '(compilation-window-height 10)
+ '(compile-command "c:\\\\windows\\\\microsoft.net\\\\framework\\\\v3.5\\\\msbuild.exe /nologo /verbosity:n /p:configuration=release")
+ '(fill-column 100)
+ '(highlight-80+-columns 100)
+ '(indent-tabs-mode nil)
+ '(midnight-mode t nil (midnight))
+ '(perl-continued-brace-offset -3)
+ '(perl-continued-statement-offset 3)
+ '(perl-indent-level 3)
+ '(sgml-basic-offset 3)
+ '(tab-width 3))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
