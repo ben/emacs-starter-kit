540444303d9a6a22097bc693218138e823be838d
benstraub <ben@straubnet.net>
2009-01-21 11:17:34 -0800

    Turned off "Save?" when C-C in LaTeX.

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(TeX-save-query nil)
  '(c-basic-offset 3)
  '(c-default-style (quote ((c-mode . "wacom") (c++-mode . "wacom") (java-mode . "java") (awk-mode . "awk") (other . "gnu"))))
  '(compilation-ask-about-save nil)
